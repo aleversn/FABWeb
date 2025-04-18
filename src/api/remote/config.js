f366bdcd9c233260fc74e41645faf6af5bc82ee7
Alever <759636538@qq.com>
2025-04-18 16:38:17 +0800

    [fix] Fix ImgInterceptor in Funbook.

--- src/api/remote/config.js
+++ src/api/remote/config.js
@@ -10,6 +10,39 @@ if (isdev) {
     ax.defaults.baseURL = "https://fb.creatorsn.com/api"
 }
 
+ax.interceptors.request.use(
+    config => {
+        if (
+            config.headers["Content-Type"].includes("x-www-form-urlencoded") ||
+            config.headers["Content-Type"].includes("multipart/form-data")
+        ) {
+            let formData = new FormData();
+            console.log(config.data)
+            if (config.data instanceof Blob){
+                formData.append("file", config.data)
+            }else{
+                for (let item in config.data) {
+                    if (config.data[item]) {
+                        if (
+                            Array.isArray(config.data[item])
+                        ) {
+                            for (let i of config.data[item]) {
+                                formData.append(item, i);
+                            }
+                        }
+                        else formData.append(item, config.data[item]);
+                    }
+                }
+            }
+            config.data = formData;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
+
 ax.interceptors.request.use(config => {
     let token = localStorage.getItem("ApiToken");
     let expired = localStorage.getItem("ApiTokenExpiredAt");
